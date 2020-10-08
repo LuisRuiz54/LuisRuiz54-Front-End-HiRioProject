c233430f4c6fdb8992dec3bca511e0166c633b54
Vladoxx <wladox@outlook.com>
2020-10-08 19:58:32 -0300

    Enlace e Componentes

--- src/app/componentes/lista-visita/lista-visita.component.ts
+++ src/app/componentes/lista-visita/lista-visita.component.ts
@@ -11,21 +11,43 @@ export class ListaVisitaComponent implements OnInit {
   id:number;
   listavisita: any;
 
-  constructor(private route: ActivatedRoute, private listaServ: ListavisitaService ) { }
+  constructor(private route: ActivatedRoute, private listaServ: ListavisitaService ) { 
+    listaServ.getTodosListavisita()
+    .subscribe(listavisita => this.listavisita = listavisita[''])
+  }
 
   ngOnInit(): void {
       
   }
 
-  ngVisitasId(){
+  ngVisitasId(): void {
     this.route.params.subscribe(params => {
       this.id = params['id']
 
       this.listaServ.getListaVisita(this.id).
       subscribe(listavisita => this.listavisita = listavisita);
+    },
+    erro => {
+      if(erro.status == 404) {
+        alert('Destino não localizado')
+      }
     });
   }
-  
 
+  salvarNovo() {
+    const novoDestino = {name:'Corcovado'}
+      this.listaServ.createListaVisita(novoDestino).
+      subscribe(destino => this.listavisita.push(destino));
   }
 
+  /* atualizacaoDestino(usuario: Usuario): Observable<Usuario> {
+    const url = this.baseURL + usuario.id;
+    return this.http.put<Usuario>(url, usuario);
+  }
+  
+  deletarDestino(id: number): Observable<any> {
+    const url = this.baseURL + id;
+    return this.http.delete(url);
+  } */
+
+}
